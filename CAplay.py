7adfed56d17c80b7b77cff395df1937b2843ffe4
JaroCamphuijsen <jjecamphuijsen@gmail.com>
2016-06-15 16:25:53 +0200

    Fixed 1D CA to incorporate variable radius resolved #2

--- CAplay.py
+++ CAplay.py
@@ -16,12 +16,14 @@ def gen_rule(r, k, ruleNum):
         rule[digits(s,k).zfill(2*r + 1)] = int(bruleNum[s])
     return rule
 
-def CA_step(oldState, rule):
+def CA_step(oldState, rule, r):
     newState = ["0"] * len(oldState)
-    newState[0] = str(rule[oldState[-1] + oldState[:2]])
-    for i in range(1,len(oldState)-1):
-        newState[i] = str(rule[oldState[i-1:i+2]])
-    newState[-1] = str(rule[oldState[-2:] + oldState[0]])
+    for n in range(r):
+        newState[n] = str(rule[oldState[-r+n:] + oldState[:r+n+1]])
+    for i in range(r, len(oldState) -  r):
+        newState[i] = str(rule[oldState[i-r:i+r+1]])
+    for m in range(1, r + 1):
+        newState[-m] = str(rule[oldState[-r-m:] + oldState[:-m+r+1]])
     return "".join(newState)
 
 def random_state(n,k):
@@ -31,18 +33,25 @@ def random_state(n,k):
 def make_colormap(k):
     return {str(i): "\033[3%dm%d\033[0m" % (1 + i, i) for i in range(k)}
 
-if __name__ == "__main__":
-    k = 6
-    r = 1
-    rule_number = randint(0, k**(k**(2*r + 1)))
-    rule = gen_rule(1, k, rule_number)
-    state = random_state(150, 2)
+def CA_print(r=1, k=2, rule_number=-1):
+    if rule_number < 0 or rule_number >= k**(k**(2*r + 1)):
+        print("No proper rule number given for this CA setting, generating random rule...")
+        sleep(3)
+        rule_number = randint(0, k**(k**(2*r + 1)))
+    rule = gen_rule(r, k, rule_number)
+    # print(rule)
+    state = random_state(150, k)
     colormap = make_colormap(k)
     try:
         while True:
             pstate = "".join([colormap[c] for c in state])
             print(pstate)
             sleep(.1)
-            state = CA_step(state, rule)
+            state = CA_step(state, rule, r)
     except KeyboardInterrupt:
         print("Rule number: %d" % rule_number)
+
+
+
+if __name__ == "__main__":
+    CA_print(1, 2, 127)
