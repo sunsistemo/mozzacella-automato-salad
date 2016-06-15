dd7be115f8b43b04f5438a8c8ea6d54c0f7e7a6c
Rahiel Kasim <rahielkasim@gmail.com>
2016-06-15 12:18:28 +0200

    formatting

--- CAplay.py
+++ CAplay.py
@@ -6,25 +6,25 @@ from time import sleep
 
 latSize = 16
 steps = 200
-evoLat = np.zeros((latSize,steps))
+evoLat = np.zeros((latSize, steps))
 init = np.zeros(latSize)
 
 def gen_rule(r, k, ruleNum):
     rule = {}
-    bruleNum = digits(ruleNum,k).zfill(k**(2*r + 1))[::-1]
-    for s in range(k**(2*r + 1)):
-        rule[digits(s,k).zfill(2*r + 1)] = int(bruleNum[s])
+    bruleNum = digits(ruleNum, k).zfill(k ** (2 * r + 1))[::-1]
+    for s in range(k ** (2 * r + 1)):
+        rule[digits(s, k).zfill(2 * r + 1)] = int(bruleNum[s])
     return rule
 
 def CA_step(oldState, rule):
     newState = ["0"] * len(oldState)
     newState[0] = str(rule[oldState[-1] + oldState[:2]])
-    for i in range(1,len(oldState)-1):
-        newState[i] = str(rule[oldState[i-1:i+2]])
+    for i in range(1, len(oldState) - 1):
+        newState[i] = str(rule[oldState[i - 1: i + 2]])
     newState[-1] = str(rule[oldState[-2:] + oldState[0]])
     return "".join(newState)
 
-def random_state(n,k):
+def random_state(n, k):
     k = "".join(map(str, range(k)))
     return "".join([choice(k) for _ in range(n)])
 
@@ -34,7 +34,7 @@ def make_colormap(k):
 if __name__ == "__main__":
     k = 6
     r = 1
-    rule_number = randint(0, k**(k**(2*r + 1)))
+    rule_number = randint(0, k ** (k ** (2 * r + 1)))
     rule = gen_rule(1, k, rule_number)
     state = random_state(150, 2)
     colormap = make_colormap(k)
