d5c92724c110c6844c7cf56b10f95760a1e03e6c
Rahiel Kasim <rahielkasim@gmail.com>
2016-06-21 10:51:48 +0200

    read / plot results

--- chef.py
+++ chef.py
@@ -3,6 +3,10 @@ import subprocess
 import json
 
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 def cook(r):
     num_bytes = 500000
     command = "python3 salad.py --bytestream -r %d | pv -S -s %d | ent -t" % (r, num_bytes)
@@ -26,6 +30,29 @@ def main():
     with open("results.json", "w") as f:
         json.dump(data, f)
 
+
+def read_results(filename):
+    results = (File_bytes, Entropy, Chi_square, Mean, Monte_Carlo_Pi, Serial_Correlation) = [[] for _ in range(6)]
+    with open(filename) as f:
+        data = json.load(f)
+    variables = {"File-bytes": File_bytes, "Entropy": Entropy, "Chi-square": Chi_square, "Mean": Mean,
+                 "Monte-Carlo-Pi": Monte_Carlo_Pi, "Serial-Correlation": Serial_Correlation}
+    for d in data:
+        for k, v in variables.items():
+            v.append(data[d][k])
+    results = [np.array(r) for r in results]
+    return results
+
+two = read_results("results-200ksamples.json")
+File_bytes, Entropy, Chi_square, Mean, Monte_Carlo_Pi, Serial_Correlation = two
+x = range(256)
+
+pi = np.abs(Monte_Carlo_Pi - np.pi)
+mean = np.abs(Mean - 255 / 2)
+
+plt.semilogy(x, mean)
+# plt.plot(x, pi)
+plt.show()
 
 if __name__ == "__main__":
     main()
