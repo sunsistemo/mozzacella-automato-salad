b88dc2ada0149df236353a3ad6e378eb6e168eaa
Martijn van Beest <martijn.vanbeest@student.uva.nl>
2016-06-22 17:18:49 +0200

    sampling rules

--- chef.py
+++ chef.py
@@ -1,6 +1,8 @@
 import multiprocessing
 import subprocess
 from functools import partial
+import random
+import sys
 
 import json
 from optparse import OptionParser
@@ -21,20 +23,32 @@ def cook(num_bytes, num_colors, rule):
 
 def main():
     parser = OptionParser()
-    parser.set_defaults(num_bytes='500000', num_colors='2')
+    parser.set_defaults(num_bytes='500000', num_colors='2', sample_size='100')
     parser.add_option('-n', '--nbytes', dest='num_bytes',
                 help='Number of bytes to stream to ent')
     parser.add_option('-c', '--color', dest='num_colors',
                 help='Number of colors')
+    parser.add_option('-s', '--sample-size', dest='sample_size',
+                help='Number of rules to sample')
     (options, args) = parser.parse_args()
     
+    k = int(options.num_colors)
+    num_bytes = int(options.num_bytes)
+    
+    sample_size = int(options.sample_size)
+    max_rule = (k**(k**3))-1
+    print("maxrule: %d" % max_rule)
+    sample_rules = [random.randint(0, max_rule) for x in range(sample_size)]
+    #print(sample_rules[x if x>max_rule for x in sample_rules])
+    print(sample_rules)
+    #sys.exit()   
     rands = [161, 225, 195, 101, 102, 135, 165, 105, 106, 75, 169, 45, 149, 86, 150, 120, 153, 90, 122, 60, 30]
     rands_paper = list(set([15, 30, 75, 90, 60, 105, 120, 150, 210, 240, 85, 149, 101, 165, 153, 105, 169, 150, 166, 170, 15, 135, 45, 165, 195, 105, 225, 150, 180, 240, 85, 86, 89, 90, 102, 105, 106, 150, 154, 170]))
     
     data = {}
     p = multiprocessing.Pool(4)
-    rules = rands
-    func = partial(cook, int(options.num_bytes), int(options.num_colors))
+    rules = sample_rules
+    func = partial(cook, num_bytes, k)
     output = p.map(func, rules)
     for i, d in enumerate(output):
         data[i] = d
