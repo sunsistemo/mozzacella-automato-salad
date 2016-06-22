c788d4bd2a607263981be4a66122b8355168b02e
Martijn van Beest <martijn.vanbeest@student.uva.nl>
2016-06-22 16:17:08 +0200

    Added color option to chef

--- chef.py
+++ chef.py
@@ -5,9 +5,8 @@ from functools import partial
 import json
 from optparse import OptionParser
 
-def cook(num_bytes, r):
-    #print("r: %d, num_bytes: %d" % (r, num_bytes))
-    command = "python3 salad.py --bytestream -r %d | pv -S -s %d | ent -t" % (r, num_bytes)
+def cook(num_bytes, num_colors, rule):
+    command = "python3 salad.py --bytestream -r %d -c %d | pv -S -s %d | ent -t" % (rule, num_colors, num_bytes)
     try:
         output = subprocess.check_output(command, shell=True)
     except (CalledProcessError, BrokenPipeError, IOError):
@@ -16,24 +15,26 @@ def cook(num_bytes, r):
     output = output.split('\n')[:-1]
     headers, values = [d.split(',')[1:] for d in output]
     headers.insert(0, 'Rule')
-    values.insert(0, r)
+    values.insert(0, rule)
     values = map(float, values)
     return {k: v for k, v in zip(headers, values)}
 
 def main():
     parser = OptionParser()
-    parser.set_defaults(num_bytes='500000')
+    parser.set_defaults(num_bytes='500000', num_colors='2')
     parser.add_option('-n', '--nbytes', dest='num_bytes',
                 help='Number of bytes to stream to ent')
+    parser.add_option('-c', '--color', dest='num_colors',
+                help='Number of colors')
     (options, args) = parser.parse_args()
     
-    rands = set([161, 225, 195, 101, 102, 135, 165, 105, 106, 75, 169, 45, 149, 86, 150, 120, 153, 90, 122, 60, 30])
-        
+    rands = [161, 225, 195, 101, 102, 135, 165, 105, 106, 75, 169, 45, 149, 86, 150, 120, 153, 90, 122, 60, 30]
+    rands_paper = list(set([15, 30, 75, 90, 60, 105, 120, 150, 210, 240, 85, 149, 101, 165, 153, 105, 169, 150, 166, 170, 15, 135, 45, 165, 195, 105, 225, 150, 180, 240, 85, 86, 89, 90, 102, 105, 106, 150, 154, 170]))
+    
     data = {}
     p = multiprocessing.Pool(4)
-    #rules = range(256)
-    rules = list(rands)
-    func = partial(cook, int(options.num_bytes))
+    rules = rands
+    func = partial(cook, int(options.num_bytes), int(options.num_colors))
     output = p.map(func, rules)
     for i, d in enumerate(output):
         data[i] = d
